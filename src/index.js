969bd6072401f41ecec9b13a49d8f21648a777a8
shelby <72360135+sheIby@users.noreply.github.com>
2021-11-20 16:01:32 -0500

    Add contract filtering
    - Mainnet projects are now filtered by official ArtBlocks contracts by default
    - Ropsten filtering now requires contracts from user
    - Hashing methods are now determined by contract
    - Allow for alternative subgraphs
    - Slight refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import query from './query.js'
+import address from './address.js'
+
+// Most recent deployments of ArtBlocks on the graph
+const SUBGRAPH_MAINNET="https://api.thegraph.com/subgraphs/name/artblocks/art-blocks";
+const SUBGRAPH_ROPSTEN="https://api.thegraph.com/subgraphs/name/artblocks/art-blocks-artist-staging";
+
+function get_subgraph_api(network) {
+  switch (network) {
+    case "mainnet":
+      return SUBGRAPH_MAINNET
+    case "ropsten":
+      return SUBGRAPH_ROPSTEN
+    default:
+      throw new Error("Supported networks include 'mainnet' and 'ropsten'.")
+  }
+}
 
 class ArtBlocks {
   
-  constructor(api="thegraph", network="mainnet") {
+  constructor(api="thegraph", 
+              network="mainnet",
+              contracts=[], 
+              subgraph_custom="") {
 
     // TODO
     // Standardized interface for Infura and other data providers
@@ -10,41 +29,59 @@ class ArtBlocks {
     if (api != "thegraph") {
       throw new Error("Supported APIs include 'thegraph'.")
     }
-    if (!["mainnet", "ropsten"].includes(network)) {
-      throw new Error("Supported networks include 'mainnet' and 'ropsten'.")
+
+    // ArtBlocks (mainnet/ropsten) or custom subgraph endpoint
+    if (subgraph_custom != "") {
+      this.subgraph = subgraph_custom
+    } else {
+      if (!["mainnet", "ropsten"].includes(network)) {
+        throw new Error("Supported networks include 'mainnet' and 'ropsten'.")
+      }
+      this.subgraph = get_subgraph_api(network)
     }
+    
+    // Default contracts
+    if (contracts.length == 0) {
+      if (network == "mainnet") {
+        contracts = [address.artblocks.mainnet.v1, address.artblocks.mainnet.v2]
+      } else {
+        throw new Error("Must provide contracts for Ropsten projects.")
+      }
+    }
+
     this.api = api
     this.network = network
+    this.contracts = contracts
   }
 
   // Query all available projects
   projects() {
-    return query.projects(this.network)
+    return query.projects(this.subgraph, this.contracts)
   }
   
   // Query project metadata
   project_metadata(id) {
-    return query.project_metadata(id, this.network)
+    return query.project_metadata(id, this.subgraph, this.contracts)
   }
 
   // Query project raw script
   project_script(id) {
-    return query.project_script(id, this.network)
+    return query.project_script(id, this.subgraph, this.contracts)
   }
 
   // Query token metadata
   token_metadata(id) {
-    return query.token_metadata(id, this.network)
+    return query.token_metadata(id, this.subgraph, this.contracts)
   }
 
   // Query token raw script with hash and dependency tags
   token_script(id) {
-    return query.token_script(id, this.network)
+    return query.token_script(id, this.subgraph, this.contracts)
   }
 
   // Query token generator html file
   token_generator(id) {
-    return query.token_generator(id, this.network)
+    return query.token_generator(id, this.subgraph, this.contracts)
   }
 }
 
