c5ab0ced15db40453ee9270f03e7314e6e31cce3
Jake Rockland <jake@artblocks.io>
2022-01-26 16:34:15 -0700

    Update address.js

--- src/address.js
+++ src/address.js
@@ -1,7 +1,7 @@
 const artblocks = {
   "mainnet": {
-   "v1": "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
-   "v2": "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270" 
+   "v0": "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
+   "v1": "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270" 
   }
 }
 
